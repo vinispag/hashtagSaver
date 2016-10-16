ce01a1dc59cbb3c436909c586dcbbed990227597
vinispag <vinicius.spagnol@gmail.com>
2016-10-16 18:56:02 -0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+angular.module('MyApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
+
+.controller('ListCtrl', function($scope, $mdDialog) {
+
+  $scope.user = {
+      name: 'John Doe',
+      email: '',
+      phone: '',
+      address: 'Mountain View, CA',
+      donation: 19.99
+    };
+  
+  $scope.people = [
+    { name: 'squid', newMessage: true },
+    { name: 'titinho', newMessage: false },
+    { name: 'LizClimo', newMessage: false }
+  ];
+
+  $scope.goToPerson = function(person, event) {
+    $mdDialog.show(
+      $mdDialog.alert()
+        .title('Navigating')
+        .textContent('Inspect ' + person)
+        .ariaLabel('Person inspect demo')
+        .ok('Neat!')
+        .targetEvent(event)
+    );
+  };
+
+  $scope.doSecondaryAction = function(event) {
+    $mdDialog.show(
+      $mdDialog.alert()
+        .title('Secondary Action')
+        .textContent('Secondary actions can be used for one click actions')
+        .ariaLabel('Secondary click demo')
+        .ok('Neat!')
+        .targetEvent(event)
+    );
+  };
+
+});
