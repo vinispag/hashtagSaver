310897b4b938587a8cc9fb76bafa061eebd648ee
vinispag <vinicius.spagnol@gmail.com>
2016-10-17 12:24:38 -0200

    Insert Instagram API
    Only can show user own photos.

---
+++ client/js/insta.js
@@ -0,0 +1,30 @@
+var url = window.location.href; 
+var token = Right(url, 51);
+num_photos = 10;
+ 
+function Right(str, n){
+    if (n <= 0)
+       return "";
+    else if (n > String(str).length)
+       return str;
+    else {
+       var iLen = String(str).length;
+       return String(str).substring(iLen, iLen - n);
+    }
+}
+
+$.ajax({
+	url: 'https://api.instagram.com/v1/users/'+ 'self' +'/media/recent',
+	dataType: 'jsonp',
+	type: 'GET',
+	data: {access_token: token , count: num_photos},
+	success: function(data){
+ 		console.log(data);
+		for( x in data.data ){
+			$('ul').append('<li><img src="'+data.data[x].images.low_resolution.url+'"></li>');
+		}
+	},
+	error: function(data){
+		console.log(data);
+	}
+});
