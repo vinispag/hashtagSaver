68bf4ca5bd308d20e577a84d9c8462268587edee
vinispag <vinicius.spagnol@gmail.com>
2016-10-16 22:15:19 -0200

    package.json
    Insert package.json

--- js/index.js
+++ js/index.js
@@ -1,20 +1,54 @@
+var express = require('express');
+var router = express.Router();
+var mongodb = require('mongodb');
+var listhash;
+
+router.get('/list', function(req, res){
+
+  var MongoClient = mongodb.MongoClient;
+
+  // Define where the MongoDB server is
+  var url = 'mongodb://localhost:27017/hashtag';
+
+  // Connect to the server
+  MongoClient.connect(url, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the Server', err);
+  } else {
+    // We are connected
+    console.log('Connection established to', url);
+
+    // Get the documents collection
+    var collection = db.collection('hashtag');
+
+    // Find all students
+    collection.find({}).toArray(function (err, result) {
+      if (err) {
+        res.send(err);
+      } else if (result.length) {
+        listhash = result;
+
+      } else {
+        res.send('No documents found');
+      }
+      //Close connection
+      db.close();
+    });
+    } 
+  });
+});
+
+module.exports = router;
+
 angular.module('MyApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
 
 .controller('ListCtrl', function($scope, $mdDialog) {
 
   $scope.user = {
-      name: 'John Doe',
       email: '',
-      phone: '',
-      address: 'Mountain View, CA',
-      donation: 19.99
     };
   
-  $scope.people = [
-    { name: 'squid', newMessage: true },
-    { name: 'titinho', newMessage: false },
-    { name: 'LizClimo', newMessage: false }
-  ];
+  $scope.people = listhash;
 
   $scope.goToPerson = function(person, event) {
     $mdDialog.show(
