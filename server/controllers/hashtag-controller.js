fb101c1eab86373316581e3de7024f9027940840
vinispag <vinicius.spagnol@gmail.com>
2016-10-19 00:40:07 -0200

    Update hashtag-controller.js

--- server/controllers/hashtag-controller.js
+++ server/controllers/hashtag-controller.js
@@ -2,9 +2,9 @@ var Hashtag = require('../models/hashtag');
 
 module.exports.create = function (req, res) {
   var hashtag = new Hashtag(req.body);
-  console.log('2');
+  console.log('2c');
   Hashtag.count({name: req.body.name, userid: req.body.userid}, function (err, count) {
-    console.log('3');
+    console.log('3c');
     if (count==0){
       hashtag.save(function (err, result) {
         res.json(result);
@@ -16,15 +16,18 @@ module.exports.create = function (req, res) {
 }
 
 module.exports.list = function (req, res) {
-  console.log('1');
+  console.log('2l');
   Hashtag.find({userid: req.params.userid}, function (err, results) {
     res.json(results);
+    console.log('3l');
   });
 }
 
 module.exports.remove = function (req, res) {
+  console.log('2r');
   Hashtag.remove({_id: req.params.hashid}, function (err, results) {
   	if (err) throw err;
     res.json(results);
+    console.log('3r');
   });
 }
